48b0e5d4f7b09ce4bc2981547c987b42c7596c27
DenisZaychikov <denis.zaj4ikov@yandex.ru>
2021-12-05 23:18:31 +0400

    Initial commit

---
+++ foodcartapp/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+
+from foodcartapp.models import Order, OrderDetails
+
+
+class OrderDetailsSerializer(ModelSerializer):
+    class Meta:
+        model = OrderDetails
+        fields = ['quantity', 'product']
+
+
+class OrderSerializer(ModelSerializer):
+    products = OrderDetailsSerializer(many=True, allow_empty=False, write_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
